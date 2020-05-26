5619477d35833c3d17af0d05042f434f70908ccd
seitaro <seitaro.hrkw@gmail.com>
2020-05-26 20:45:42 +0900

    orders_controllerを残して一通り削除

--- app/controllers/shops_controller.rb
+++
@@ -1,6 +0,0 @@
-class ShopsController < ApplicationController
-  def show
-    @shop = Shop.first
-    render json: @shop
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :orders
-  resource :shop, only: %w[show]
-  resources :products, only: %w[index show create update destroy]
-  resource :users, only: %w[index create update destroy]
+  resources :orders, only: %w[index create]
 end
