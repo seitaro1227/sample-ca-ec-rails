bbc46b1b4b7159d8f23cf7e54030080226676fb0
seitaro <seitaro.hrkw@gmail.com>
2020-06-10 00:24:08 +0900

    check_stock.rbの処理を修正

--- app/interactors/check_stock.rb
+++ app/interactors/check_stock.rb
@@ -3,8 +3,9 @@ class CheckStock
   include Interactor
 
   def call
-    product_id_count = context.products.map { |item| [item[:id], item[:count]] }.to_h
-    valid_stock = Product.where(id: product_id_count.keys).map { |product|
+    # TODO: paramsObjectを使う
+    product_id_count = context.param_products.map { |item| [item[:id], item[:count]] }.to_h
+    valid_stock = context.products.map { |product|
       product.count >= product_id_count[product.id]
     }.all?
 
--- spec/interactors/check_stock_spec.rb
+++ spec/interactors/check_stock_spec.rb
@@ -1,14 +1,37 @@
 require 'spec_helper'
 
 RSpec.describe CheckStock, type: :interactor do
-  let(:product) { create(:product) }
-  let(:params) { {user_id: 1, products: [{id: product.id, count: product.count}]} }
-  subject(:context) { CheckStock.call(params) }
-  describe '.call' do
+  let(:user) { build_stubbed(:user) }
+  let(:products) { build_stubbed_list(:product, 3) }
+  let(:params) {
+    {
+      param_user_id: user.id,
+      param_products: products.pluck(:id).map { {id: _1, count: 3} },
+      products: products
+    }
+  }
+  let(:invalid_params) {
+    {
+        param_user_id: user.id,
+        param_products: products.pluck(:id).map { {id: _1, count: 99} },
+        products: products
+    }
+  }
 
+  describe '.call' do
     context '正常系' do
+      subject(:context) { CheckStock.call(params) }
       it 'succeeds ' do
         expect(context).to be_a_success
+      end
+    end
+    context '異常系' do
+      subject(:invalid_context) { CheckStock.call(invalid_params) }
+      it 'fails' do
+        expect(invalid_context).to be_a_failure
+      end
+      it 'message' do
+        expect(invalid_context.message).to eq '在庫がない'
       end
     end
   end
